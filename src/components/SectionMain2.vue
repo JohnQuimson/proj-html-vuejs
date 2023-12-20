9e65e9a1a3918fef237d7f38959e69fdc8268c3e
JohnQuimson <john.quimson777@gmail.com>
2023-12-21 00:33:11 +0100

    fix: grid img

--- src/components/SectionMain2.vue
+++ src/components/SectionMain2.vue
@@ -66,16 +66,89 @@ export default {
     </p>
     <div class="news-container">
       <div class="cont-sx">
-        <div class="elem-orizz">1</div>
-        <div class="cont-sx-centro">
-          <div class="elem-vert">2</div>
-          <div class="elem-vert">3</div>
+        <!-- Elem 1 -->
+        <div class="elem-orizz">
+          <div class="cont-img">
+            <img
+              :src="`../../public/img/images/${latestNews.card1.img}.jpg`"
+              alt=""
+            />
+          </div>
+          <div class="cont-info">
+            <h4>{{ latestNews.card1.title }}</h4>
+            <p>{{ latestNews.card1.paragraph }}</p>
+          </div>
         </div>
-        <div class="elem-orizz">4</div>
+        <!-- SEZIONE sinistra-centrale -->
+        <div class="cont-sx-centro">
+          <!-- Elem 2 -->
+          <div class="elem-vert">
+            <div class="cont-img">
+              <img
+                :src="`../../public/img/images/${latestNews.card2.img}.jpg`"
+                alt=""
+              />
+            </div>
+            <div class="cont-info">
+              <h4>{{ latestNews.card2.title }}</h4>
+              <p>{{ latestNews.card2.paragraph }}</p>
+            </div>
+          </div>
+          <!-- Elem 3 -->
+          <div class="elem-vert">
+            <div class="cont-img">
+              <img
+                :src="`../../public/img/images/${latestNews.card3.img}.jpg`"
+                alt=""
+              />
+            </div>
+            <div class="cont-info">
+              <h4>{{ latestNews.card3.title }}</h4>
+              <p>{{ latestNews.card3.paragraph }}</p>
+            </div>
+          </div>
+        </div>
+        <!-- Elem 4 -->
+        <div class="elem-orizz">
+          <div class="cont-img">
+            <img
+              :src="`../../public/img/images/${latestNews.card4.img}.jpg`"
+              alt=""
+            />
+          </div>
+          <div class="cont-info">
+            <h4>{{ latestNews.card4.title }}</h4>
+            <p>{{ latestNews.card4.paragraph }}</p>
+          </div>
+        </div>
       </div>
       <div class="cont-dx">
-        <div class="elem-vert">5</div>
-        <div class="elem-vert">6</div>
+        <!-- Elem 5 -->
+        <div class="elem-vert">
+          <div class="cont-img">
+            <img
+              :src="`../../public/img/images/${latestNews.card5.img}.jpg`"
+              alt=""
+            />
+          </div>
+          <div class="cont-info">
+            <h4>{{ latestNews.card5.title }}</h4>
+            <p>{{ latestNews.card5.paragraph }}</p>
+          </div>
+        </div>
+        <!-- Elem 6 -->
+        <div class="elem-vert">
+          <div class="cont-img">
+            <img
+              :src="`../../public/img/images/${latestNews.card6.img}.jpg`"
+              alt=""
+            />
+          </div>
+          <div class="cont-info">
+            <h4>{{ latestNews.card6.title }}</h4>
+            <p>{{ latestNews.card6.paragraph }}</p>
+          </div>
+        </div>
       </div>
     </div>
     <BigButton />
@@ -109,22 +182,17 @@ export default {
   }
 
   .news-container {
-    color: white;
-    width: 60%;
+    width: 70%;
     display: flex;
-    height: 100vh;
+    height: 150vh;
     gap: 20px;
+    margin: 60px 0 200px;
 
     .cont-sx {
-      width: 60%;
+      width: 65%;
       display: flex;
       flex-direction: column;
       gap: 20px;
-
-      .elem-orizz {
-        border: 1px solid white;
-        height: 20%;
-      }
 
       .cont-sx-centro {
         display: flex;
@@ -132,22 +200,76 @@ export default {
         gap: 20px;
 
         .elem-vert {
-          border: 2px solid white;
           width: 50%;
         }
       }
     }
 
     .cont-dx {
-      border: 1px solid white;
-      width: 40%;
+      width: 35%;
       display: flex;
       flex-direction: column;
-      gap: 20px;
+      gap: 50px;
 
       .elem-vert {
-        border: 1px solid white;
         height: 50%;
+      }
+    }
+
+    .elem-orizz {
+      height: 25%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+
+      .cont-img {
+        height: 77%;
+
+        img {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+        }
+      }
+    }
+
+    .elem-vert {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+
+      .cont-img {
+        height: 90%;
+
+        img {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+        }
+      }
+    }
+
+    .cont-info {
+      height: 80px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      align-items: start;
+      background-color: #323844;
+      padding: 15px;
+
+      h4 {
+        margin: 0;
+        color: #ec4958;
+        font-size: 18px;
+        font-weight: 700;
+      }
+
+      p {
+        margin: 0;
+        width: 100%;
+        text-align: start;
+        font-size: 14px;
       }
     }
   }
